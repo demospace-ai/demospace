3c78e9ee2199f3be3c7f1c43337243e588a8af1e
Nick Fiacco <nicholas.r.fiacco@gmail.com>
2024-05-29 16:07:47 -0700

    Setup basic project structure

---
+++ components/Footer.tsx
@@ -0,0 +1,7 @@
+export const Footer: React.FC = () => {
+  return (
+    <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
+      <p>Powered by Demospace</p>
+    </footer>
+  );
+};
