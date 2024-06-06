6450ecb5f546628c2bb6fb4e0021aafad85a468b
Nick Fiacco <nicholas.r.fiacco@gmail.com>
2024-06-06 16:06:23 -0700

    Updates

--- components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export const Footer: React.FC = () => {
-  return (
-    <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
-      <p>Powered by Demospace</p>
-    </footer>
-  );
-};
--- components/assistant/Demi.tsx
+++ components/assistant/Demi.tsx
@@ -4,33 +4,15 @@ import { Button } from "@/components/button/Button";
 import { LoadingSpinner } from "@/components/loading/Loading";
 import { SlideDisplay } from "@/components/slides/Slide";
 import { Message, useAssistant } from "ai/react";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { v4 as uuid } from "uuid";
 
 const channelId = uuid();
 
 export const Demi: React.FC = () => {
-  const [threadId, setThreadId] = useState<string>("");
   const [connecting, setConnecting] = useState(false);
   const [connected, setConnected] = useState(false);
-  const [paused, setPaused] = useState(false);
-  const [sourceBuffer, setSourceBuffer] = useState<SourceBuffer>();
-  const [audio, setAudio] = useState<HTMLAudioElement>();
   const { messages, append, status } = useAssistant({ api: "/api/assistant" });
-
-  useEffect(() => {
-    const mediaSource = new MediaSource();
-    const audio = new Audio();
-    audio.src = URL.createObjectURL(mediaSource);
-    setAudio(audio);
-
-    function sourceOpen() {
-      const sourceBuffer = mediaSource.addSourceBuffer("audio/mpeg");
-      setSourceBuffer(sourceBuffer);
-    }
-
-    mediaSource.addEventListener("sourceopen", sourceOpen);
-  }, []);
 
   const startCallInline = async () => {
     setConnecting(true);
@@ -39,20 +21,6 @@ export const Demi: React.FC = () => {
     setConnecting(false);
     setConnected(true);
   };
-
-  const endCall = () => {
-    setConnected(false);
-  };
-
-  const pause = () => {
-    setPaused(true);
-  };
-
-  const unpause = () => {
-    setPaused(false);
-  };
-
-  // messages.forEach((message) => console.log(message.content));
 
   return (
     <>
@@ -90,35 +58,3 @@ export const Demi: React.FC = () => {
     </>
   );
 };
-
-async function addToBuffer(
-  sourceBuffer: SourceBuffer,
-  reader: ReadableStreamDefaultReader<Uint8Array>,
-  queue: Uint8Array[],
-) {
-  sourceBuffer.addEventListener(
-    "updateend",
-    function () {
-      if (queue.length > 0) {
-        sourceBuffer.appendBuffer(queue.shift()!);
-      }
-    },
-    false,
-  );
-
-  let first = true;
-  while (true) {
-    const { done, value } = await reader.read();
-    if (done) {
-      break;
-    }
-
-    if (first) {
-      first = false;
-      sourceBuffer.appendBuffer(value!);
-      continue;
-    } else {
-      queue.push(value!);
-    }
-  }
-}
