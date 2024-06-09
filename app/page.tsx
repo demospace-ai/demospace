465674a4baf183f74c346b0584eb459269f3174c
Nick Fiacco <nicholas.r.fiacco@gmail.com>
2024-06-09 12:19:18 -0400

    One main only

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,9 @@ import { Demi } from "@/components/assistant/Demi";
 
 export default async function Index() {
   return (
-    <main className="flex flex-1 flex-col gap-6 w-full h-full items-center justify-center pb-64">
+    <div className="flex flex-1 flex-col gap-6 w-full h-full items-center justify-center pb-64">
       <h2 className="font-bold text-4xl mb-4 text-center">Demo</h2>
       <Demi />
-    </main>
+    </div>
   );
 }
